4ba35f93aa4fde509ca94a9e44deb1a237a30282
Sophia <theothersophie@users.noreply.github.com>
2020-11-13 20:02:23 -0500

    Add files via upload

---
+++ search_audible.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         MAM Search Audible Button
+// @version      0.1
+// @description  Add a search audible button on the MAM upload page.
+// @author       theothersophie
+// @match        https://www.myanonamouse.net/tor/upload.php*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    var f = document.getElementById("fillJson");
+    var s = document.createElement("button");
+    var searchUrl = "https://www.audible.com/search?keywords=";
+    var nodeTorrName = document.querySelectorAll('[name="tor[torrentName]"')[0];
+
+    var torrName = nodeTorrName.value;
+    /* Remove the .torrent and all non alphanumeric characters except spaces and replace all whitespace with + */
+    torrName = torrName.replace(/\.torrent/g, "").replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s+/g, "+");
+    searchUrl += torrName;
+
+    s.setAttribute("id", "searchAudible");
+    s.setAttribute("onclick", "parent.open('" + searchUrl + "')");
+    s.innerHTML = "Search on Audible.com";
+    f.insertAdjacentElement("afterend", s);
+})();
