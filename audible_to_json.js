7ee18f3bcd70619f7bf0438bdbac7029551cc91f
Sophia <theothersophie@users.noreply.github.com>
2020-11-29 10:19:35 -0500

    Update audible_to_json.js
    tags separated by . instead of ,

--- audible_to_json.js
+++ audible_to_json.js
@@ -64,7 +64,7 @@ var d = document.querySelector(".productPublisherSummary>div>div>span").innerHTM
 /* In order: Remove excess whitespace, replace double quotes, add line break after every paragraph, and every list */
 d = d.replace(/\s+/g, " ").replace(/"/g, '\\"').replace(/<\/p>/g,"</p><br>").replace(/<\/ul>/g, "</ul><br>");
 
-var tags = "Duration: " + runtime + ", inAudible True Decrypt, " + categories;
+var tags = "Duration: " + runtime + ". inAudible True Decrypt. " + categories;
 
 var json = {
     "authors" : authors,
